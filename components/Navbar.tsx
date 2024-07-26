619156c2622923866052bc527bdd3989a9f4107b
AD <154968000+dangol-anish@users.noreply.github.com>
2024-07-26 20:34:02 +0545

    added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { IoTimer } from "react-icons/io5";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex items-center justify-between p-5">
+        <div className="flex items-center gap-2">
+          <IoTimer size={25} />
+          <h1>Time</h1>
+        </div>
+
+        <Button>Logout</Button>
+      </div>
+    </>
+  );
+}
